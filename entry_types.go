5b529b259bad164f6863c29fed24232d8e5734b7
reus <reusee@gmail.com>
2014-08-26 18:41:04 +0800

    clean-ups

---
+++ entry_types.go
@@ -0,0 +1,157 @@
+package srs
+
+// audio to word
+
+type AudioToWordEntry struct {
+	WordIndex int
+	word      *Word
+}
+
+func (e *AudioToWordEntry) IsTheSame(entry IsEntry) bool {
+	if t, ok := entry.(*AudioToWordEntry); ok && t.WordIndex == e.WordIndex {
+		return true
+	}
+	return false
+}
+
+func (e *AudioToWordEntry) Load(data *Data) {
+	e.word = data.Words[e.WordIndex]
+}
+
+func (e *AudioToWordEntry) Lesson() string {
+	return lessonPattern.FindStringSubmatch(e.word.AudioFile)[0]
+}
+
+func (e *AudioToWordEntry) PracticeOrder() int {
+	return 1
+}
+
+func (e *AudioToWordEntry) Practice(ui UI, input Input) PracticeResult {
+	ui("set-hint", "playing...")
+	playAudio(e.word.AudioFile)
+	ui("set-hint", "press any key to show answer")
+	input()
+	ui("set-text", e.word.Text)
+repeat:
+	ui("set-hint", "press G to levelup, T to reset level, Space to repeat")
+read_key:
+	key := input()
+	switch key {
+	case 'g':
+		return LEVEL_UP
+	case 't':
+		return LEVEL_RESET
+	case ' ':
+		ui("set-hint", "playing...")
+		playAudio(e.word.AudioFile)
+		ui("set-hint", "")
+		goto repeat
+	case 'q':
+		ui("set-hint", "exit...")
+		return EXIT
+	default:
+		goto read_key
+	}
+	return NONE
+}
+
+// word to audio
+
+type WordToAudioEntry struct {
+	WordIndex int
+	word      *Word
+}
+
+func (e *WordToAudioEntry) IsTheSame(entry IsEntry) bool {
+	if t, ok := entry.(*WordToAudioEntry); ok && t.WordIndex == e.WordIndex {
+		return true
+	}
+	return false
+}
+
+func (e *WordToAudioEntry) Load(data *Data) {
+	e.word = data.Words[e.WordIndex]
+}
+
+func (e *WordToAudioEntry) Lesson() string {
+	return lessonPattern.FindStringSubmatch(e.word.AudioFile)[0]
+}
+
+func (e *WordToAudioEntry) PracticeOrder() int {
+	return 3
+}
+
+func (e *WordToAudioEntry) Practice(ui UI, input Input) PracticeResult {
+	ui("set-text", e.word.Text)
+	ui("set-hint", "press any key to play audio")
+	input()
+repeat:
+	ui("set-hint", "playing...")
+	playAudio(e.word.AudioFile)
+	ui("set-hint", "press G to levelup, T to reset level, Space to repeat")
+read_key:
+	key := input()
+	switch key {
+	case 'g':
+		return LEVEL_UP
+	case 't':
+		return LEVEL_RESET
+	case ' ':
+		goto repeat
+	case 'q':
+		ui("set-hint", "exit...")
+		return EXIT
+	default:
+		goto read_key
+	}
+	return NONE
+}
+
+// sentence
+
+type SentenceEntry struct {
+	AudioFile string
+}
+
+func (e *SentenceEntry) IsTheSame(entry IsEntry) bool {
+	if t, ok := entry.(*SentenceEntry); ok && t.AudioFile == e.AudioFile {
+		return true
+	}
+	return false
+}
+
+func (e *SentenceEntry) Load(*Data) {}
+
+func (e *SentenceEntry) Lesson() string {
+	return lessonPattern.FindStringSubmatch(e.AudioFile)[0]
+}
+
+func (e *SentenceEntry) PracticeOrder() int {
+	return 2
+}
+
+func (e *SentenceEntry) Practice(ui UI, input Input) PracticeResult {
+	ui("set-hint", "playing...")
+	playAudio(e.AudioFile)
+repeat:
+	ui("set-hint", "press G to levelup, T to reset level, Space to repeat")
+read_key:
+	key := input()
+	switch key {
+	case 'g':
+		return LEVEL_UP
+	case 't':
+		return LEVEL_RESET
+	case ' ':
+		ui("set-hint", "playing...")
+		playAudio(e.AudioFile)
+		ui("set-hint", "")
+		goto repeat
+	case 'q':
+		ui("set-hint", "exit...")
+		return EXIT
+	default:
+		goto read_key
+	}
+	return NONE
+}
