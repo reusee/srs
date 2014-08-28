fb18c049fd31b8bc1fd55a0a5623f5232f4ab968
reus <reusee@gmail.com>
2014-08-28 16:51:20 +0800

    fix

--- main.go
+++ main.go
@@ -122,9 +122,12 @@ func main() {
 	if len(os.Args) > 1 {
 		cmd = os.Args[1]
 	}
+	var args []string
+	if len(os.Args) > 2 {
+		args = os.Args[2:]
+	}
 
 	// commands
-	args := os.Args[2:]
 	switch cmd {
 	case "complete":
 		data.Complete(args)
